a37cd60062ea03a997d1cd2643111fc86cc59bfe
niltor <zpty@outlook.com>
2021-05-06 09:47:23 +0800

    auth service

---
+++ src/WebApp/Services/AuthService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Services
+{
+    public class AuthService
+    {
+        public bool IsLogin { get; set; } = false;
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public AuthService()
+        {
+        }
+
+
+        public void SignOut()
+        {
+            
+        }
+
+    }
+}
