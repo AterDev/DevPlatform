9e50221dbbdcc045ef380fe3a63750a6a62b667b
NilTor <zpty@outlook.com>
2021-08-31 14:32:38 +0800

    update ef migrations

--- src/EntityFrameworkCore/Migrations/20210831063029_ThirdNewsLength.Designer.cs
+++ src/EntityFrameworkCore/Migrations/20210831063029_ThirdNewsLength.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace EntityFrameworkCore.Migrations
 {
     [DbContext(typeof(ContextBase))]
-    partial class ContextBaseModelSnapshot : ModelSnapshot
+    [Migration("20210831063029_ThirdNewsLength")]
+    partial class ThirdNewsLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
