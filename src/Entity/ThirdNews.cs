a4ce6b613927ca2fdd92d9cbb59f88423dd48372
NilTor <zpty@outlook.com>
2021-08-31 14:29:41 +0800

    fix:news field length

--- src/Entity/ThirdNews.cs
+++ src/Entity/ThirdNews.cs
@@ -4,9 +4,27 @@
     {
         private string _description;
         private string _content;
+        private string _title;
+        private string _url;
+        private string _provider;
+        private string _category;
 
-        [MaxLength(100)]
-        public string Title { get; set; }
+        [MaxLength(200)]
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (value.Length > 200)
+                {
+                    _title = value.Substring(0, 200);
+                }
+                else
+                {
+                    _title = value;
+                }
+            }
+        }
 
         [MaxLength(5000)]
         public string Description
@@ -24,12 +42,39 @@
                 }
             }
         }
-        [MaxLength(200)]
-        public string Url { get; set; }
+        [MaxLength(300)]
+        public string Url
+        {
+            get => _url;
+            set
+            {
+                if (value.Length > 300)
+                {
+                    _url = value.Substring(0, 300);
+                }
+                else
+                {
+                    _url = value;
+                }
+            }
+        }
         [MaxLength(300)]
         public string ThumbnailUrl { get; set; }
-        [MaxLength(30)]
-        public string Provider { get; set; }
+        [MaxLength(50)]
+        public string Provider {
+            get => _provider;
+            set
+            {
+                if (value.Length > 300)
+                {
+                    _provider = value.Substring(0, 50);
+                }
+                else
+                {
+                    _provider = value;
+                }
+            }
+        }
         public DateTime DatePublished { get; set; }
         [MaxLength(8000)]
         public string Content
@@ -47,7 +92,20 @@
                 }
             }
         }
-        [MaxLength(40)]
-        public string Category { get; set; }
+        [MaxLength(50)]
+        public string Category {
+            get => _category;
+            set
+            {
+                if (value.Length > 50)
+                {
+                    _category = value.Substring(0, 50);
+                }
+                else
+                {
+                    _category = value;
+                }
+            }
+        }
     }
 }
