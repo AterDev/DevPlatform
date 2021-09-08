b3c0390cd1a2b6f603ef4e8ba5c51ee3188e74a9
NilTor <zpty@outlook.com>
2021-09-08 16:00:08 +0800

    code clean:remove unused namespace

--- app/DevNews/MainPage.xaml.cs
+++ app/DevNews/MainPage.xaml.cs
@@ -3,20 +3,11 @@ using DevNews.Share;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 
 namespace DevNews
