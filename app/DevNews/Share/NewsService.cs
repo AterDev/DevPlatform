b4c40b8803778a42bcf6d3a3a81cb57b17de55d4
NilTor <zpty@outlook.com>
2021-09-08 18:44:47 +0800

    update

--- app/DevNews/Share/NewsService.cs
+++ app/DevNews/Share/NewsService.cs
@@ -12,8 +12,8 @@ namespace DevNews.Share
     {
 
         HttpClient httpClient;
-        //static readonly string BaseUrl = "http://39.104.70.89:9002/";
-        static readonly string BaseUrl = "http://localhost:5002/";
+        static readonly string BaseUrl = "http://39.104.70.89:9002/";
+        //static readonly string BaseUrl = "http://localhost:5002/";
         public NewsService()
         {
             httpClient = new HttpClient();
