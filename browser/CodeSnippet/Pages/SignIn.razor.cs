4c983dadab445651b169fbc8b93b8e6898af5a37
NilTor <zpty@outlook.com>
2021-10-20 18:40:17 +0800

    try authorization

---
+++ browser/CodeSnippet/Pages/SignIn.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Text.Json;
+using AntDesign;
+using Microsoft.AspNetCore.Components;
+
+namespace CodeSnippet.Pages
+{
+    public partial class SignIn
+    {
+        private Model model = new Model();
+
+        [Inject]
+        public MessageService message { get; set; }
+
+        public SignIn()
+        {
+        }
+
+        private void OnFinish(EditContext editContext)
+        {
+            if (model.Username == "niltor" && model.Password == "11100916")
+            {
+                message.Success("login success!");
+            }
+            else
+            {
+                message.Error("login failed");
+            }
+        }
+
+        private void OnFinishFailed(EditContext editContext)
+        {
+
+            Console.WriteLine($"Failed:{JsonSerializer.Serialize(model)}");
+        }
+    }
+    public class Model
+    {
+        [Required, DisplayName("UserName")]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- browser/CodeSnippet/Program.cs
+++ browser/CodeSnippet/Program.cs
@@ -1,4 +1,5 @@
 using CodeSnippet;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,7 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAntDesign();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
