86ea9638dafaca6c6aca62cde5f6a8151f2853a7
NilTor <zpty@outlook.com>
2022-01-27 19:24:32 +0800

    add application controller

--- src/Microservice/IdentityServer/Models/OpenIddictApplication.cs
+++ src/Microservice/IdentityServer/Models/OpenIddictApplication.cs
@@ -1,30 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using OpenIddict.Abstractions;
 
 namespace IdentityServer;
 
-public partial class OpenIddictApplication
+public partial class OpenIddictApplication: OpenIddictApplicationDescriptor
 {
-    public OpenIddictApplication()
-    {
-        OpenIddictAuthorizations = new HashSet<OpenIddictAuthorization>();
-        OpenIddictTokens = new HashSet<OpenIddictToken>();
-    }
-
-    public Guid Id { get; set; }
-    public string? ClientId { get; set; }
-    public string? ClientSecret { get; set; }
-    public string? ConcurrencyToken { get; set; }
-    public string? ConsentType { get; set; }
-    public string? DisplayName { get; set; }
-    public string? DisplayNames { get; set; }
-    public string? Permissions { get; set; }
-    public string? PostLogoutRedirectUris { get; set; }
-    public string? Properties { get; set; }
-    public string? RedirectUris { get; set; }
-    public string? Requirements { get; set; }
-    public string? Type { get; set; }
-
-    public virtual ICollection<OpenIddictAuthorization> OpenIddictAuthorizations { get; set; }
-    public virtual ICollection<OpenIddictToken> OpenIddictTokens { get; set; }
 }
