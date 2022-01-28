e6ffb5f41a6c63cb33a680850085f8a011d315b8
NilTor <zpty@outlook.com>
2022-01-28 17:36:17 +0800

    init angular  admin project

--- src/WebApp/webapp/src/app/share/models/comment-filter.model.ts
+++
@@ -1,6 +0,0 @@
-import { FilterBase } from './filter-base.model';
-export interface CommentFilter extends FilterBase {
-  articleId?: string | null;
-  accountId?: string | null;
-
-}
--- src/WebApp/webapp/src/typings.d.ts
+++
@@ -1,5 +0,0 @@
-// Empty typings for the editor used in the app to satisfy the TS compiler in the strict mode:
-declare module '@ckeditor/ckeditor5-build-decoupled-document' {
-  const ClassicEditorBuild: any;
-  export = ClassicEditorBuild;
-}
