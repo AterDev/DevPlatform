ea5f29b8099b28549ecb42c6da98792ff1c833f7
niltor <zpty@outlook.com>
2022-02-06 14:41:34 +0800

    role and user datastore 的默认实现

--- src/Http.Application/DataStore/RoleDataStore.cs
+++ src/Http.Application/DataStore/RoleDataStore.cs
@@ -17,12 +17,58 @@ public class RoleDataStore : IDataStore<Role, RoleUpdateDto, RoleFilter, RoleIte
         _query = _db.AsQueryable();
     }
 
-    public Task<Role?> FindAsync(Guid id) => throw new NotImplementedException();
-    public Task<List<RoleItemDto>> FindAsync(RoleFilter filter) => throw new NotImplementedException();
-    public Task<PageResult<RoleItemDto>> FindWithPageAsync(RoleFilter filter) => throw new NotImplementedException();
-    public Task<bool> DeleteAsync(Guid id) => throw new NotImplementedException();
-    public Task<Role> AddAsync(Role form) => throw new NotImplementedException();
-    public Task<Role?> UpdateAsync(Guid id, RoleUpdateDto dto) => throw new NotImplementedException();
-    public Task<bool> Exist(Guid id) => throw new NotImplementedException();
-    public bool Any(Func<Role, bool> predicate) => throw new NotImplementedException();
+    public async Task<Role?> FindAsync(Guid id) => await _db.FindAsync(id);
+    public async Task<List<RoleItemDto>> FindAsync(RoleFilter filter)
+    {
+        return await _query.OrderByDescending(d => d.CreatedTime)
+          .Select<Role, RoleItemDto>()
+          .Skip((filter.PageIndex - 1) * filter.PageSize)
+          .Take(filter.PageSize)
+          .ToListAsync();
+    }
+    public async Task<PageResult<RoleItemDto>> FindWithPageAsync(RoleFilter filter)
+    {
+        var count = _query.Count();
+        if (filter.PageIndex < 1) filter.PageIndex = 1;
+        if (filter.PageSize < 0) filter.PageSize = 0;
+        var data = await _query.OrderByDescending(d => d.CreatedTime)
+            .Skip((filter.PageIndex - 1) * filter.PageSize)
+            .Take(filter.PageSize)
+            .Select<Role, RoleItemDto>()
+            .ToListAsync();
+        return new PageResult<RoleItemDto>
+        {
+            Count = count,
+            Data = data,
+            PageIndex = filter.PageIndex
+        };
+    }
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var data = await _db.FindAsync(id);
+        if (data == null) { return false; }
+        _db.Remove(data);
+        return (await _context.SaveChangesAsync() > 0);
+    }
+    public async Task<Role> AddAsync(Role data)
+    {
+        _db.Add(data);
+        await _context.SaveChangesAsync();
+        return data;
+    }
+    public async Task<Role?> UpdateAsync(Guid id, RoleUpdateDto dto)
+    {
+        var data = await _db.FindAsync(id);
+        if (data == null) { return null; }
+        // merge data and save 
+        data.Merge(dto);
+        await _context.SaveChangesAsync();
+        return data;
+    }
+    public async Task<bool> Exist(Guid id)
+    {
+        var data = await _db.FindAsync(id);
+        return data != null;
+    }
+    public bool Any(Func<Role, bool> predicate) => _db.Any(predicate);
 }
--- src/Http.Application/DataStore/UserDataStore.cs
+++ src/Http.Application/DataStore/UserDataStore.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using Core.Utils;
+using Microsoft.OData.Edm;
 using Share.Models.UserDtos;
 
 namespace Http.Application.DataStore;
@@ -16,12 +19,55 @@ public class UserDataStore : IDataStore<User, UserUpdateDto, UserFilter, UserIte
         _query = _db.AsQueryable();
     }
 
-    public Task<User?> FindAsync(Guid id) => throw new NotImplementedException();
-    public Task<List<UserItemDto>> FindAsync(UserFilter filter) => throw new NotImplementedException();
-    public Task<PageResult<UserItemDto>> FindWithPageAsync(UserFilter filter) => throw new NotImplementedException();
-    public Task<bool> DeleteAsync(Guid id) => throw new NotImplementedException();
-    public Task<User> AddAsync(User form) => throw new NotImplementedException();
-    public Task<User?> UpdateAsync(Guid id, UserUpdateDto dto) => throw new NotImplementedException();
-    public Task<bool> Exist(Guid id) => throw new NotImplementedException();
-    public bool Any(Func<User, bool> predicate) => throw new NotImplementedException();
+    public async Task<User?> FindAsync(Guid id) => await _db.FindAsync(id);
+    public async Task<List<UserItemDto>> FindAsync(UserFilter filter)
+    {
+        return await _query.OrderByDescending(d => d.CreatedTime)
+          .Select<User, UserItemDto>()
+          .Skip((filter.PageIndex - 1) * filter.PageSize)
+          .Take(filter.PageSize)
+          .ToListAsync();
+    }
+    public async Task<PageResult<UserItemDto>> FindWithPageAsync(UserFilter filter)
+    {
+        var count = _query.Count();
+        if (filter.PageIndex < 1) filter.PageIndex = 1;
+        if (filter.PageSize < 0) filter.PageSize = 0;
+        var data = await _query.OrderByDescending(d => d.CreatedTime)
+            .Skip((filter.PageIndex - 1) * filter.PageSize)
+            .Take(filter.PageSize)
+            .Select<User, UserItemDto>()
+            .ToListAsync();
+        return new PageResult<UserItemDto>
+        {
+            Count = count,
+            Data = data,
+            PageIndex = filter.PageIndex
+        };
+    }
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var data = await _db.FindAsync(id);
+        if (data == null) { return false; }
+        _db.Remove(data);
+        return (await _context.SaveChangesAsync() > 0);
+    }
+    public async Task<User> AddAsync(User data) {
+        _db.Add(data);
+        await _context.SaveChangesAsync();
+        return data;
+    }
+    public async Task<User?> UpdateAsync(Guid id, UserUpdateDto dto) {
+        var data = await _db.FindAsync(id);
+        if (data == null) { return null; }
+        // merge data and save 
+        data.Merge(dto);
+        await _context.SaveChangesAsync();
+        return data;
+    }
+    public async Task<bool> Exist(Guid id) {
+        var data = await _db.FindAsync(id);
+        return data != null;
+    }
+    public bool Any(Func<User, bool> predicate) => _db.Any(predicate);
 }
