352f0dd417bcbc696b252b4d718dfc6bf718e65a
NilTor <zpty@outlook.com>
2022-02-08 16:30:56 +0800

    sidenav open&close

--- src/WebApp/Admin/src/app/components/navigation/navigation.component.ts
+++ src/WebApp/Admin/src/app/components/navigation/navigation.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  events: string[] = [];
+  opened = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(): void {
+    this.opened = !this.opened;
   }
 
 }
