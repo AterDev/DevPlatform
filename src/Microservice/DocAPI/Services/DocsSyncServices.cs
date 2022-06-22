f3062a96707ad9b79062119a257c76b6c72cff2f
niltor <zpty@outlook.com>
2022-06-22 18:16:07 +0800

    添加github同步服务类

---
+++ src/Microservice/DocAPI/Services/DocsSyncServices.cs
@@ -0,0 +1,111 @@
+﻿using Octokit;
+
+namespace DocAPI.Services;
+/// <summary>
+/// 文档同步，从github仓库同步
+/// </summary>
+public class DocsSyncServices
+{
+    private readonly DocsContext _context;
+    public GitHubClient Client { get; set; }
+    private ApiOptions _options;
+    private Octokit.User? User { get; set; }
+
+    public DocsSyncServices(DocsContext context)
+    {
+        _context = context;
+        Client = new GitHubClient(new ProductHeaderValue("ater.docs"));
+        _options = new ApiOptions()
+        {
+            PageSize = 100,
+        };
+    }
+
+    /// <summary>
+    /// 设置person access token
+    /// </summary>
+    /// <param name="pat"></param>
+    public async Task SetPATAsync(string pat)
+    {
+        User = await Client.User.Current();
+        Client.Credentials = new Credentials(pat); ;
+    }
+
+    /// <summary>
+    /// 获取仓库列表
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IReadOnlyList<Repository>> GetPublicRepositories()
+    {
+        //var orgs = await Client.Organization.GetAllForCurrent();
+        return await Client.Repository.GetAllForCurrent();
+    }
+
+    /// <summary>
+    /// 同步
+    /// <param name="repositoryId">仓库id</param>
+    /// </summary>
+    public async Task SyncDocsAsync(long? repositoryId)
+    {
+        if (repositoryId == null)
+        {
+            repositoryId = _context.WebConfigs.Select(c => c.RepositoryId).FirstOrDefault();
+        }
+        if (repositoryId != null)
+        {
+            // 1 获取语言目录
+            var languageDirs = await GetRepositoryRootAsync(repositoryId.Value);
+            foreach (var lang in languageDirs)
+            {
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// 递归处理
+    /// </summary>
+    /// <param name="repositoryId"></param>
+    /// <param name="contents"></param>
+    /// <param name="ignoreFile"></param>
+    /// <param name="parent"></param>
+    public void CreateDocsRecursion(long repositoryId, IReadOnlyList<RepositoryContent> contents, RepositoryContent? parent, bool ignoreFile = false)
+    {
+        var dirs = contents.Where(c => c.Type.Equals("dir")).ToList();
+        if (dirs.Any())
+        {
+            dirs.ForEach(async d =>
+            {
+                // TODO:创建目录
+                // 获取子目录，递归调用
+                var childContents = await GetFilesAsync(repositoryId, d.Path);
+                CreateDocsRecursion(repositoryId, childContents, d);
+            });
+        }
+        if (!ignoreFile)
+        {
+            var files = contents.Where(c => c.Type.Equals("file")).ToList();
+            if (files.Any())
+            {
+                // TODO:创建文件
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// 获取根目录文件夹,语言目录
+    /// </summary>
+    /// <param name="repositoryId"></param>
+    protected async Task<List<RepositoryContent>> GetRepositoryRootAsync(long repositoryId)
+    {
+        var rootContent = await Client.Repository.Content.GetAllContents(repositoryId);
+        return rootContent.Where(r => r.Type.Equals("dir")).ToList();
+    }
+
+    protected async Task<IReadOnlyList<RepositoryContent>> GetFilesAsync(long repositoryId, string path)
+    {
+        return await Client.Repository.Content.GetAllContents(repositoryId, path);
+    }
+
+}
